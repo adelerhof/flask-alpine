bafbd4a95ec1d2ff685bf965cb44348d18fa5d65
Erik Adelerhof <e@adelerhof.eu>
2019-10-09 17:18:07 +0200

    extra functie

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 from flask import Flask
+import datetime
+buildDT = datetime.datetime.now()
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
 
-    return 'Hello World!?'
+    return 'Hello World!?\n'
+    return '(str(currentDT))\n'
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
