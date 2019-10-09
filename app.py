0d6269b9a62342deebd127527476f357e305d784
Erik Adelerhof <e@adelerhof.eu>
2019-10-09 17:55:45 +0200

    .

--- app.py
+++ app.py
@@ -1,18 +1,24 @@
 from flask import Flask, escape, request
-import datetime
-buildDT = datetime.datetime.now()
+
 
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
+# def hello():
 
-    # return 'Hello World!?\n'
-    # name = request.args.get("name", "World")
-    return f'(str(currentDT))\n'
-
-    # return f'Hello, {escape(name)}!'
-
-
+#     return 'Hello World!?\n'
+def time_feed():
+    def generate():
+        yield datetime.now().strftime("%Y.%m.%d|%H:%M:%S")  # return also will work
+    return Response(generate(), mimetype='text') 
+ 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
+
+
+
+# @app.route('/time_feed')
+# def time_feed():
+#     def generate():
+#         yield datetime.now().strftime("%Y.%m.%d|%H:%M:%S")  # return also will work
+#     return Response(generate(), mimetype='text') 
