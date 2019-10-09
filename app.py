c7390e19959c33c34b669ffbfa42e02dbbd28c3d
Erik Adelerhof <e@adelerhof.eu>
2019-10-09 18:06:43 +0200

    .

--- app.py
+++ app.py
@@ -4,13 +4,14 @@ from flask import Flask, escape, request
 app = Flask(__name__)
 
 @app.route('/')
-# def hello():
+def hello():
 
-#     return 'Hello World!?\n'
-def time_feed():
-    def generate():
-        yield datetime.now().strftime("%Y.%m.%d|%H:%M:%S")  # return also will work
-    return Response(generate(), mimetype='text') 
+    # return 'Hello World!?\n'
+    return '"And Shepherds we shall be\nFor thee, my Lord, for thee.\nPower hath descended forth from Thy hand\nOur feet may swiftly carry out Thy commands.\nSo we shall flow a river forth to Thee\nAnd teeming with souls shall it ever be.\nIn Nomeni Patri Et Fili Spiritus Sancti."'
+# def time_feed():
+#     def generate():
+#         yield datetime.now().strftime("%Y.%m.%d|%H:%M:%S")  # return also will work
+#     return Response(generate(), mimetype='text') 
  
 if __name__ == "__main__":
     app.run(host="0.0.0.0", debug=True)
