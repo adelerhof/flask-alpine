1417cfe55be7a17dfa8c99b3b6c9bfb6862c94ce
Erik Adelerhof <e@adelerhof.eu>
2019-10-09 18:31:41 +0200

    .

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 def hello():
 
     # return 'Hello World!?\n'
-    return '"And Shepherds we shall be\nFor thee, my Lord, for thee.\nPower hath descended forth from Thy hand\nOur feet may swiftly carry out Thy commands.\nSo we shall flow a river forth to Thee\nAnd teeming with souls shall it ever be.\nIn Nomeni Patri Et Fili Spiritus Sancti."\n\n\n\n https://www.youtube.com/watch?v=TBLgTBpW4zs \n\n\n    '
+    return '"And Shepherds we shall be\nFor thee, my Lord, for thee.\nPower hath descended forth from Thy hand\nOur feet may swiftly carry out Thy commands.\nSo we shall flow a river forth to Thee\nAnd teeming with souls shall it ever be.\nIn Nomeni Patri Et Fili Spiritus Sancti."\n\n\n\nhttps://www.youtube.com/watch?v=TBLgTBpW4zs\n\n\n'
 # def time_feed():
 #     def generate():
 #         yield datetime.now().strftime("%Y.%m.%d|%H:%M:%S")  # return also will work
